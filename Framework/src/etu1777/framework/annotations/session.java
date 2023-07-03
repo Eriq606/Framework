6c2ca0cccf3d423aabc2eba83770a79d8949b157
Eriq606 <erohweltall@gmail.com>
2023-07-03 06:34:57 +0300

    rectification sprint 12

---
+++ Framework/src/etu1777/framework/annotations/session.java
@@ -0,0 +1,9 @@
+package etu1777.framework.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface session {
+    
+}
